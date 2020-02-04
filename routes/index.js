d4ab23a600dde9eef4e6da72d70c6055397816a1
tlelas <tlelas@foundationmedicine.com>
2020-02-04 11:24:54 +0100

    first bitcoin commit

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios')
+const Client = require('bitcoin-core');
+
+// const client = new Client({ network: 'regtest' });
+const client = new Client({
+  username: 'student',
+  password: 'WYVyF5DTERJASAiIiYGg4UkRH',
+  port: 8332,
+  host:'blockchain.oss.unist.hr',
+});
+
+var blockexplorer = require('blockchain.info/blockexplorer').usingNetwork(3)
+
+/* GET home page. */
+router.get('/getBlock/:id',async function(req, res, next) {
+  console.log('ss', req.params.id)
+  try {
+    // await client.getBlockchainInfo().then((help) => console.log(help));
+    const resp = await client.getBlock(req.params.id)
+    console.log('resppp',resp)
+    res.status(200).send(resp);
+    //00000000000000f226feaa06c7c5618e92837fe0959cc7e03c392af8f738f076
+  } catch (err) {
+    console.log('errssr',err)
+    res.status(200).send('daa');
+  }
+});
+
+router.get('/getMempool',async function(req, res, next) {
+  console.log('ss', req.params.id)
+  try {
+    // await client.getBlockchainInfo().then((help) => console.log(help));
+    const resp = await client.getBlockchainInfo()
+    console.log('resppp',resp)
+    res.status(200).send(resp);
+    //00000000000000f226feaa06c7c5618e92837fe0959cc7e03c392af8f738f076
+  } catch (err) {
+    console.log('errssr',err)
+    res.status(200).send('daa');
+  }
+});
+
+module.exports = router;
