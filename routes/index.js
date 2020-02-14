d24e88d5fc92800c888fa8cd4387a226b47ab78f
tlelas <tlelas@foundationmedicine.com>
2020-02-14 11:53:10 +0100

    all routes

--- routes/index.js
+++ routes/index.js
@@ -8,34 +8,54 @@ const client = new Client({
   username: 'student',
 });
 
-var blockexplorer = require('blockchain.info/blockexplorer').usingNetwork(3)
-
-/* GET home page. */
 router.get('/getBlock/:id',async function(req, res, next) {
-  console.log('ss', req.params.id)
   try {
-    // await client.getBlockchainInfo().then((help) => console.log(help));
     const resp = await client.getBlock(req.params.id)
-    console.log('resppp',resp)
     res.status(200).send(resp);
-    //00000000000000f226feaa06c7c5618e92837fe0959cc7e03c392af8f738f076
   } catch (err) {
-    console.log('errssr',err)
-    res.status(200).send('daa');
+    res.status(500).send({error: []});
   }
 });
 
 router.get('/getMempool',async function(req, res, next) {
-  console.log('ss', req.params.id)
   try {
-    // await client.getBlockchainInfo().then((help) => console.log(help));
-    const resp = await client.getBlockchainInfo()
-    console.log('resppp',resp)
+    const resp = await client.getRawMempool(verbose=true)
     res.status(200).send(resp);
-    //00000000000000f226feaa06c7c5618e92837fe0959cc7e03c392af8f738f076
   } catch (err) {
-    console.log('errssr',err)
-    res.status(200).send('daa');
+    res.status(500).send({error: []});
+  }
+});
+
+router.get('/transaction/:id',async function(req, res, next) {
+  try {
+    const resp = await client.getRawTransaction(req.params.id)
+    const newresp = await client.decodeRawTransaction(resp)
+    res.status(200).send(newresp);
+  } catch (err) {
+    res.status(500).send({error: []});
+  }
+});
+
+router.get('/txOut/:id',async function(req, res, next) {
+  try {
+    const resp = await client.getTxOut(req.params.id,1)
+    res.status(200).send(resp);
+  } catch (err) {
+    res.status(500).send({error: []});
+  }
+});
+
+router.get('/getBlockChain',async function(req, res, next) {
+  try {
+    let blockChainArray = []
+    for (i = 0; i<10;i++) {
+      const blockHash = await client.getBlockHash(i)
+      const blockObject = await client.getBlock(blockHash)
+      blockChainArray.push(blockObject)
+    }
+    res.status(200).send(blockChainArray);
+  } catch (err) {
+    res.status(500).send({error: []});
   }
 });
 
